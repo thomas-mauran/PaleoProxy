3e941b64860ec5ee7145a46ef8fb4b99d4d70261
Thomas Mauran <thomasmauran@yahoo.com>
2025-05-25 21:39:10 +0200

    feat: working dynamic !
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- src/main.go
+++ src/main.go
@@ -9,6 +9,7 @@ import (
 	"math/rand"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/docker/docker/api/types/events"
@@ -48,73 +49,49 @@ func main(){
 	fmt.Println("Paleo Proxy is up !")
 
 	if !isDynamic {
+		// Not dynamic, using the config file
+		config, err := ReadConfig(configFilePath)
 
-	config, err := ReadConfig(configFilePath)
-
-	if err != nil {
-		fmt.Println("An error occured when trying to read the config file: ", err)
-		return
-	}
-
-	services := config.Services
-
-	handlers := map[string]http.HandlerFunc{}
-	// We make a map for all our routes creating handlers
-	for _, s := range services {
-		service := s
-		if !service.Enabled {
-			continue
-		}
-		port := service.Port
-
-		// Subdomain of the current service
-		subdomain := service.Subdomain + "." + domain + ":" + fmt.Sprint(port)
-
-		// Function to handle the passing of the request to the service
-		handler := func (w http.ResponseWriter, req *http.Request){
-			// Endpoints 
-			endpoints := service.Endpoints
-
-			// Pick a random endpoint
-			randomEndpoint := endpoints[rand.Intn(len(endpoints))]
-
-			// Ip and port of the service we chose
-			ip := randomEndpoint.Ip
-
-			// The url of the service to contact
-			serviceUrl := "http://" + ip + ":" + fmt.Sprint(port)
-			
-			res , err := http.Get(serviceUrl)
-			log.Println("routing request to: " + serviceUrl)
-			if err != nil {
-				io.WriteString(w, "[ERROR], an error occured when trying to reach GET" + serviceUrl)
-			}
-			defer res.Body.Close()
-
-			body, err := io.ReadAll(res.Body)
-
-			if err != nil {
-				io.WriteString(w, "[ERROR], failed to read response body from "+ serviceUrl)
-				return
-			}
-			w.Write(body)
-		}
-		handlers[subdomain] = handler
-	}
-
-
-	// Each time we get a request we check if the domain matches to route the traffic to it
-	mainHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		host := req.Host
-
-		if handler, found := handlers[host]; found {
-			handler(w, req)
+		if err != nil {
+			fmt.Println("An error occured when trying to read the config file: ", err)
 			return
 		}
-	})
 
-	log.Fatal(http.ListenAndServe(":8080", mainHandler))
+		services := config.Services
+
+		handlers := map[string]http.HandlerFunc{}
+		// We make a map for all our routes creating handlers
+		for _, s := range services {
+			service := s
+			if !service.Enabled {
+				continue
+			}
+			port := service.Port
+
+			// Subdomain of the current service
+			subdomain := service.Subdomain + "." + domain + ":" + fmt.Sprint(port)
+
+			handlers[subdomain] = CreateHandler(service)
+		}
+
+		// Each time we get a request we check if the domain matches to route the traffic to it
+		mainHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			host := req.Host
+
+			if handler, found := handlers[host]; found {
+				handler(w, req)
+				return
+			}
+		})
+
+		log.Fatal(http.ListenAndServe(":8080", mainHandler))
 	} else {
+		// Crea
+		var (
+			handlers = make(map[string]http.HandlerFunc)
+			handlersMu sync.RWMutex
+		)
+		// Dynamic config, listening to docker events
 		fmt.Println("hey this is dynamic !")
 
 		cli, err := client.NewClientWithOpts(client.FromEnv)
@@ -124,26 +101,115 @@ func main(){
 
 		eventChannel, errChannel := cli.Events(context.Background(), events.ListOptions{})
 
-		for {
-			select {
-			case event := <-eventChannel:
-				// for key, value := range event.Actor.Attributes {
-				// 	fmt.Printf("%s: %s\n", key, value)
-				// }			
-				_, gotPaleoLabel := event.Actor.Attributes["paleo-subdomain"]
 
-				if event.Action == "start" && gotPaleoLabel {
-					fmt.Println("start", event.Action)
-				} else if event.Action == "kill" && gotPaleoLabel {
-					fmt.Println("kill", event.Action)
-				}
+		// We start the infinite event listener loop with a gorouting 
+		go DynamicListen(cli, eventChannel, errChannel, handlers, &handlersMu, domain)
 
-			case err := <-errChannel:
+		mainHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			host := req.Host
+
+			handlersMu.RLock()
+			handler, found := handlers[host]
+			handlersMu.RUnlock()
+
+			for k := range handlers {
+				fmt.Println(" -", k)
+			}			
+			if found {
+				fmt.Println("FOUNNNDNDNDNDND")
+				handler(w, req)
+			} else {
+				http.NotFound(w, req)
+			}
+		})
+		log.Fatal(http.ListenAndServe(":8080", mainHandler))
+	}
+	
+}
+
+// Method to loop infinitely on the docker events and add handlers to the map for events with the paleo-subdomain label
+func DynamicListen(cli *client.Client, eventChannel <- chan events.Message, errChannel <-chan error, handlers map[string]http.HandlerFunc, handlersMu *sync.RWMutex, domain string) {
+	for {
+		select {
+		case event := <-eventChannel:
+			subdomain, gotPaleoLabel := event.Actor.Attributes["paleo-subdomain"]
+			if !gotPaleoLabel {
+				continue
+			}
+			fmt.Println("new event")
+			if event.Action == "start" && gotPaleoLabel {
+				containerID := event.ID
+				containerJSON, err := cli.ContainerInspect(context.Background(), containerID)
 				if err != nil {
-					panic(err)
+					log.Printf("Failed to inspect container %s: %v", containerID, err)
+					continue
 				}
+
+				// Get the first network's IP address ! This might be a bad idea in the end idk how it works with multiple networks
+				var ipAddress string
+				for _, netSettings := range containerJSON.NetworkSettings.Networks {
+					ipAddress = netSettings.IPAddress
+					break
+				}
+				// Build service from container info (assuming paleo-subdomain, port labels exist)
+				service := Service{
+					Subdomain: subdomain,
+					Port: 8080, // For now we hardocode this to the 8080 port 
+					Endpoints: []Endpoint{
+						{Ip: ipAddress},
+					},
+					Enabled: true,
+				}
+
+				handler := CreateHandler(service)
+
+				handlersMu.Lock()
+				handlers[subdomain + "." + domain + ":8080"] = handler
+				handlersMu.Unlock()
+
+				log.Printf("Added handler for %s\n", subdomain)
+			} else if event.Action == "kill" && gotPaleoLabel {
+				fmt.Println("kill", event.Action)
+			}
+
+		case err := <-errChannel:
+			if err != nil {
+				panic(err)
 			}
 		}
 	}
+}
 
+func CreateHandler(service Service) http.HandlerFunc {
+	// Function to handle the passing of the request to the service
+	return func (w http.ResponseWriter, req *http.Request){
+		// Endpoints 
+		endpoints := service.Endpoints
+
+		// Pick a random endpoint
+		randomEndpoint := endpoints[rand.Intn(len(endpoints))]
+
+		// Ip and port of the service we chose
+		ip := randomEndpoint.Ip
+
+		fmt.Println("ip", ip)
+
+		// The url of the service to contact
+		serviceUrl := "http://" + ip + ":" + fmt.Sprint(service.Port)
+		
+		res , err := http.Get(serviceUrl)
+		log.Println("routing request to: " + serviceUrl)
+		if err != nil {
+			io.WriteString(w, "[ERROR], an error occured when trying to reach GET" + serviceUrl)
+		}
+		defer res.Body.Close()
+
+		body, err := io.ReadAll(res.Body)
+
+		if err != nil {
+			io.WriteString(w, "[ERROR], failed to read response body from "+ serviceUrl)
+			return
+		}
+		w.Write(body)
+	}
 }
